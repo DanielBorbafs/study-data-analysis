551858d84e9c78b097224fe950eb5550ae3a483e
Danielborbafs <dev.danielborba@gmail.com>
2024-08-21 22:25:57 -0300

    bug: validar cpf

--- formRegister/api/buscaCep.js
+++ formRegister/api/buscaCep.js
@@ -2,20 +2,26 @@ async function buscaEndereco(cep) {
     try {
         const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const consultaCEPConvertida = await consultaCEP.json();
-
-        if (consultaCEPConvertida) {
-            throw new Error('CEP Não existente!');
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP Não existente!');
         }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var estado = document.getElementById('estado')
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida)
         return consultaCEPConvertida;
     } catch (erro) {
-        console.log(erro.message);
-        return null;  // Retorna null em caso de erro para não quebrar o array de respostas
+        console.log(erro);
     }
 }
 
-let ceps = ['29176675', '20010140', '29178677'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
-
-Promise.all(conjuntoCeps).then(respostas => {
-    console.log(respostas);
-});
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
