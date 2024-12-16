9df2602bf3b9692d1791309fcb0e9b32b77bddc8
DanielBorbafs <dev.danielborba@gmail.com>
2024-12-16 18:26:44 -0300

    token login auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,20 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const auth = (req, res, next) => {
-  const token = req.heder.authoriztation;
+  const token = req.headers.authorization;
+
+  console.log(token);
+  if (!token) {
+    return res.status('401').json({ message: 'Acesso negado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET);
+    req.userId = decoded.id;
+  } catch (err) {
+    return res.status(401).json({ message: 'Token Inválido' });
+  }
+
   next();
 };
 
