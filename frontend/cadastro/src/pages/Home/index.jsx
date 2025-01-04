c8ea4932802f0ed67ee9a41f919d26b3f728c89f
DanielBorbafs <dev.danielborba@gmail.com>
2025-01-04 14:06:29 -0300

    axios config

--- frontend/cadastro/src/pages/Home/index.jsx
+++ frontend/cadastro/src/pages/Home/index.jsx
@@ -1,9 +1,42 @@
 import './style.css';
+import Delete from '../../assets/delete.svg';
 
 function Home() {
+  const users = [
+    {
+      id: '21221',
+      name: 'Daniel',
+      email: 'daniel@email.com',
+    },
+    {
+      id: '221251',
+      name: 'aline',
+      email: 'aline@email.com',
+    },
+  ];
   return (
-    <div>
-      <h1> Olá react </h1>
+    <div className="container">
+      <form>
+        <h1> Cadastro de Usuários</h1>
+        <input placeholder="nome" name="nome" type="text" />
+        <input placeholder="email" name="e-mail" type="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome:<span>{user.name}</span>
+            </p>
+            <p>
+              Email:<span>{user.email}</span>
+            </p>
+          </div>
+          <button>
+            <img src={Delete} />
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
