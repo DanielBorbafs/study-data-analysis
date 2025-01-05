84e24edc81678ca0f13673720bf39955e52d5c05
DanielBorbafs <dev.danielborba@gmail.com>
2025-01-05 20:26:03 -0300

    post route create users

--- frontend/cadastro/src/pages/Home/index.jsx
+++ frontend/cadastro/src/pages/Home/index.jsx
@@ -1,26 +1,51 @@
+import { useEffect, useState, useRef } from 'react'; // Toda vez que a pagina abrir ele servir para chamar a função getusers
 import './style.css';
 import Delete from '../../assets/delete.svg';
+import api from '../../services/api';
 
 function Home() {
-  const users = [
-    {
-      id: '21221',
-      name: 'Daniel',
-      email: 'daniel@email.com',
-    },
-    {
-      id: '221251',
-      name: 'aline',
-      email: 'aline@email.com',
-    },
-  ];
+  const [users, setUsers] = useState([]); // Use state para armazenar os usuários
+  const inpuName = useRef();
+  const inpuEmail = useRef();
+  const inputPass = useRef();
+
+  async function getUsers() {
+    try {
+      const response = await api.get('/usuarios');
+      setUsers(response.data); // Atualiza o estado com a resposta da API
+    } catch (error) {
+      console.error('Erro ao buscar usuários:', error);
+    }
+  }
+
+  async function createUsers() {
+    await api.post('/cadastro', {
+      name: inpuName.current.value,
+      email: inpuEmail.current.value,
+      password: inputPass.current.value,
+    });
+    getUsers();
+  }
+
+  useEffect(() => {
+    getUsers(); // Chama a função para carregar os usuários
+  }, []);
+
   return (
     <div className="container">
       <form>
         <h1> Cadastro de Usuários</h1>
-        <input placeholder="nome" name="nome" type="text" />
-        <input placeholder="email" name="e-mail" type="email" />
-        <button type="button">Cadastrar</button>
+        <input placeholder="nome" name="nome" type="text" ref={inpuName} />
+        <input placeholder="email" name="e-mail" type="email" ref={inpuEmail} />
+        <input
+          placeholder="senha"
+          name="password"
+          type="password"
+          ref={inputPass}
+        />
+        <button type="button" onClick={createUsers}>
+          Cadastrar
+        </button>
       </form>
       {users.map((user) => (
         <div key={user.id} className="card">
